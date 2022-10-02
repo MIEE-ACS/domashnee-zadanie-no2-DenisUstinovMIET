152b33f37c176acc78b7eee03a94c7e7c54ca327
DenisUstinovMIET <denisustinovmiet@mail.ru>
2022-10-02 14:55:12 +0300

    Update Program.cs

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -11,10 +11,9 @@ namespace DZ2
     {
         static double Segment1(double x)//расчет первого сегмента графика 
         {
-            double y = 1; 
+            double y = 1;
             return y;
         }
-
         static double Segment2(double x, double r)//расчет второго сегмента графика 
         { //центр окружности 
             double a = -1; double y;
@@ -22,19 +21,16 @@ namespace DZ2
             y = Math.Sqrt(Math.Pow(r, 2) - Math.Pow(x - a, 2));
             return -y;
         }
-
         static double Segment3(double x)//расчет третьего сегмента графика 
         { //центр окружности 
             double y = -2;
             return y;
         }
-
         static double Segment4(double x)//расчет четвертого сегмента графика 
         { //центр окружности
             double y = x - 4;
             return y;
         }
-
         static void Main(string[] args)
         {
             Console.Write("Все точки функции с шагом аргумента 0,2:\n");
@@ -65,11 +61,9 @@ namespace DZ2
             double r;
             Console.Write("Введите радиус: ");
             while (!double.TryParse(Console.ReadLine(), out r))
-            {
-                Console.Write("Ошибка ввода! Введите радиус в виде числа:");
-            }
+                Console.Write("Введите радиус в виде числа:");
 
-            if (r<2) r = 0;
+            if (r < 2) r = 0;//того чтобы, при меньших значениях был разрыв второго рода
 
             while (true)
             {
@@ -79,7 +73,6 @@ namespace DZ2
                 {
                     Console.Write("Ошибка ввода! Введите аргумент функции в виде числа:");
                 }
-
                 if (x == -3)
                 {
                     Console.WriteLine("y({0:0.00}) = {1:0.00}", x, Segment1(x));
@@ -94,7 +87,10 @@ namespace DZ2
                 }
                 else if (x < -1)
                 {
-                    Console.WriteLine("y({0:0.00}) = {1:0.00}", x, Segment2(x, r));
+                    if (r == 0)
+                        Console.WriteLine("y({0:0.00}) = Разрыв второго рода", x);
+                    else
+                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, Segment2(x, r));
                 }
                 else if (x < 2)
                 {
@@ -108,10 +104,7 @@ namespace DZ2
                 {
                     Console.WriteLine("Функция не определена");
                 }
-
             }
         }
     }
 }
-
-
